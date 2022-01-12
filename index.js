c0ecd0d38b0f7a39084cdfe5e7d3c387794e2a70
manish <manishvyas393@gmail.com>
2022-01-12 17:26:26 +0530

    refactored naming conventions

--- index.js
+++ index.js
@@ -18,26 +18,26 @@ async function getJobDetails() {
                   const ElementSelector = "body > main > div > div > div > div:nth-child(4) > table > tbody > tr";
                   const elemetSelect = "body > main > div > div > div.mx-auto"
                   const nextPage = "body > main > div > div > div > div.px-3 > div > nav.pagy-bootstrap-nav > ul";
-                  $(nextPage).each((idx, el) => {
-                        let nextPageNo = parseInt($(el).find("li.page-item.next>a").attr().href.replace("/?page=", ""))
-                        $(ElementSelector).each(async (idx, el) => {
-                              let role = $(el).find("h2").text().trim()
-                              let company = $(el).find("h3").text().trim()
-                              let salary = $(el).find("td:nth-child(2)>p").text().trim()
-                              let location = $(el).find("td:nth-child(3)>p").text().trim()
-                              let updatedOn = $(el).find("td:nth-child(5)>div>p").text().trim()
-                              let url = $(el).find("a").attr().href
+                  $(nextPage).each((linkIndx, linkEl) => {
+                        let nextPageNo = parseInt($(linkEl).find("li.page-item.next>a").attr().href.replace("/?page=", ""))
+                        $(ElementSelector).each(async (parentIdx, parentEl) => {
+                              let role = $(parentEl).find("h2").text().trim()
+                              let company = $(parentEl).find("h3").text().trim()
+                              let salary = $(parentEl).find("td:nth-child(2)>p").text().trim()
+                              let location = $(parentEl).find("td:nth-child(3)>p").text().trim()
+                              let updatedOn = $(parentEl).find("td:nth-child(5)>div>p").text().trim()
+                              let url = $(parentEl).find("a").attr().href
                               let newUrl = url.replace('..', '');
                               let source = `https://web3.career${newUrl}`
                               const { data } = await axios.get(source)
                               const $$ = cheerio.load(data)
 
-                              $$(elemetSelect).each(async (id, ell) => {
-                                    let caption = $(ell).find("header > div:nth-child(1) > h1").text().replace("\n", "")
-                                    let requirements = $(ell).find("div.text-dark-grey-text.p-2.p-md-0 > p").text().replace("\n", "")
-                                    let roleResponsibilities = $(ell).find("ul:nth-child(5)").text().replace("\n", "")
-                                    let qualification = $(ell).find("ul:nth-child(7)").text().replace("\n", "")
-                                    let extraSkills = $(ell).find("ul:nth-child(9)").text().replace("\n", "")
+                              $$(elemetSelect).each(async (childIdx, childEl) => {
+                                    let caption = $(childEl).find("header > div:nth-child(1) > h1").text().replace("\n", "")
+                                    let requirements = $(childEl).find("div.text-dark-grey-text.p-2.p-md-0 > p").text().replace("\n", "")
+                                    let roleResponsibilities = $(childEl).find("ul:nth-child(5)").text().replace("\n", "")
+                                    let qualification = $(childEl).find("ul:nth-child(7)").text().replace("\n", "")
+                                    let extraSkills = $(childEl).find("ul:nth-child(9)").text().replace("\n", "")
                                     await Jobs.findOneAndDelete({ role, company, location, salary }).then(job => {
                                           if (job) {
                                                 job.remove().then(() => {
