228d5e02ac0a13236f9c6031aabcf6d826ea0503
manish <manishvyas393@gmail.com>
2022-01-12 15:07:39 +0530

    data gets fetched from next page

--- index.js
+++ index.js
@@ -8,22 +8,26 @@ dotenv.config()
 mongoose.connect(process.env.Db_Url, () => console.log("connected"))
 async function getJobDetails() {
       try {
-
-            for (let i = 1; i < 10; i++) {
+            let n=1;
+            for (let i = 1; i <=n; i++) {
                   const url = `https://web3.career/?page=${i}`
                   const { data } = await axios.get(url, { ignoreWhitespace: true })
                   const $ = cheerio.load(data)
                   const ElementSelector = "body > main > div > div > div > div:nth-child(4) > table > tbody > tr";
                   const elemetSelect = "body > main > div > div > div.mx-auto"
+                  const nextPage = "body > main > div > div > div > div.px-3 > div > nav.pagy-bootstrap-nav > ul > li.page-item.active > a";
+                  let a = $(nextPage).text()
                   $(ElementSelector).each(async (idx, el) => {
                         let role = $(el).find("h2").text().trim()
                         let company = $(el).find("h3").text().trim()
                         let salary = $(el).find("td:nth-child(2) > p").text().trim()
                         let location = $(el).find("td:nth-child(3) > p").text().trim()
+
                         let url = $(el).find("a").attr().href
                         let newUrl = url.replace('..', '');
                         const { data } = await axios.get(`https://web3.career${newUrl}`)
                         const $$ = cheerio.load(data)
+                        
                         $$(elemetSelect).each(async (id, ell) => {
                               let caption = $(ell).find("header > div:nth-child(1) > h1").text().replace("\n", "")
                               let requirements = $(ell).find("div.text-dark-grey-text.p-2.p-md-0 > p").text().replace("\n", "")
@@ -54,6 +58,7 @@ async function getJobDetails() {
                         })
 
                   })
+                  n += a
             }
 
 
