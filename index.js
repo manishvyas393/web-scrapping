bd8a8adffdadba472053f105965e03e3165a2b36
manish <manishvyas393@gmail.com>
2022-01-12 11:58:22 +0530

    fetched data from links

--- index.js
+++ index.js
@@ -13,36 +13,35 @@ async function getJobDetails() {
             const elemetSelect = "body > main > div > div > div.mx-auto"
 
             $(ElementSelector).each(async (idx, el) => {
-                  let jobRole = $(el).find("h2").text().trim()
+                  let role = $(el).find("h2").text().trim()
                   let company = $(el).find("h3").text().trim()
                   let salary = $(el).find("td:nth-child(2) > p").text().trim()
-                  let jobType = $(el).find("td:nth-child(3) > p").text().trim()
-                  await Jobs.findOne({ jobRole, company,jobType,salary }).then(job => {
-                        if (job) {
-                              console.log(job)
-                        }
-                        else {
-                              new Jobs({
-                                    jobRole, company, jobType, salary
-                              }).save().then(() => {
-                                    console.log("saved")
-                              })
-                        }
-                              
+                  let location = $(el).find("td:nth-child(3) > p").text().trim()
+                  let url = $(el).find("a").attr().href
+                  let newUrl = url.replace('..', '');
+                  const { data } = await axios.get(`https://web3.career${newUrl}`)
+                  const $$ = cheerio.load(data)
+                  $$(elemetSelect).each(async (id, ell) => {
+                        let caption = $(ell).find("header > div:nth-child(1) > h1").text()
+                        let requirements = $(ell).find("div.text-dark-grey-text.p-2.p-md-0 > p").text().trim()
+                        let roleResponsibilities = $(ell).find("ul:nth-child(5)").text().trim()
+                        let qualification = $(ell).find("ul:nth-child(7)").text().trim()
+                        let extraSkills = $(ell).find("ul:nth-child(9)").text().trim()
+                        await Jobs.findOne({ role, company, location, salary }).then(job => {
+                              if (job) {
+                                    console.log(job)
+                              }
+                              else {
+                                    new Jobs({
+                                          role, company, location, salary, caption, requirements, roleResponsibilities, qualification, extraSkills
+                                    }).save().then(() => {
+                                          console.log("saved")
+                                    })
+                              }
+
+                        })
                   })
-
-                  // let url = $(el).find("a").attr().href
-                  // let newUrl = url.replace('..', '');
-                  // const { data } = await axios.get(`https://web3.career${newUrl}`)
-                  // const $$ = cheerio.load(data)
-                  // $$(elemetSelect).each((id, ell) => {
-                  //       let value = $$(ell).text()
-                  //       console.log(value)
-                  // })
-
-
             })
-
       } catch (error) {
             console.error(error)
       }
--- models/jobs.js
+++ models/jobs.js
@@ -1,18 +1,35 @@
 import mongoose from "mongoose"
 
 const jobsDetailSchema = new mongoose.Schema({
-      jobRole: {
+      role: {
             type: String,
       },
       company: {
             type: String,
       },
-      jobType: {
-            type:String
+      location: {
+            type: String
       },
       salary: {
             type: String,
+      },
+      caption: {
+            type:String,
+      },
+      requirements: {
+            type: String
+      },
+      roleResponsibilities: {
+            type: String,
+      },
+      qualification: {
+            type: String,
+      },
+      extraSkills: {
+            type: String,
       }
+
+
 })
-const jobDetails = mongoose.model("job_detail",jobsDetailSchema)
+const jobDetails = mongoose.model("job_detail", jobsDetailSchema)
 export default jobDetails
