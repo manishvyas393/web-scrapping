3eccdf6f63555d07259b4e1f4ef5485c8bc41ecf
manish <manishvyas393@gmail.com>
2022-01-18 17:23:19 +0530

    data fetching from https://crypto.jobs

--- crypto-jobs/index.js
+++ crypto-jobs/index.js
@@ -3,18 +3,47 @@ import * as cheerio from "cheerio"
 
 async function getCryptoJobs() {
 
-      const url = "https://cryptocurrencyjobs.co/web3/"
-      const { data } = await axios.get(url)
-      const $ = cheerio.load(data)
-      const element = "#find-a-job > div> ul>li>div";
-      $(element).each((parentIdx, parentChild) => {
-            let role = $(parentChild).find("h2").text().trim()
-            let company = $(parentChild).find("h3").first().text().trim()
-            let proffesion = $(parentChild).find("div>div>h4>a").text().trim()
-            let jobType = $(parentChild).find("div > ul:nth-child(1) > li > h4 > a").text().trim()
-            let contract = $(parentChild).find("div > ul:nth-child(5) > li > h4").text().replace("Contract", "Contract-")
-            let salary = $(parentChild).find(".leading-relaxed>span").text().trim()
-            let date = $(parentChild).find(".inline-block>span").text().replace(",", "")
-      })
+      try {
+            let currentPage = 1;
+
+            for (let i = currentPage; i <= currentPage; i++) {
+                  const url = `https://crypto.jobs/?page=${currentPage}`
+                  const { data } = await axios.get(url)
+                  const $ = cheerio.load(data)
+                  const element = "#app > div > div > div.col-md-8 > div > div.panel-body > table > tbody > tr";
+                  const nextpage = "#app > div > div > div.col-md-8 > div > div.panel-footer > ul>li:last-child"
+
+                  $(nextpage).each((linkIdx, Linkel) => {
+                        console.log("data fetched from:", url);
+                        let nextPageNo = parseInt($(Linkel).find("a.page-link").attr().href.replace("http://crypto.jobs?page=", ""))
+                        console.log(nextPageNo)
+
+                        $(element).each(async (parentIdx, parentChild) => {
+                              if (parentIdx > 0) {
+                                    let role = $(parentChild).find("td:nth-child(2) > a > p").text()
+                                    let company = $(parentChild).find("td:nth-child(2) > a > span").text()
+                                    let type = $(parentChild).find("td:nth-child(2) > a > div > small > span:nth-child(1)").text().trim()
+                                    let field = $(parentChild).find("td:nth-child(2) > a > div > small > span:nth-child(2)").text().trim()
+                                    let location = $(parentChild).find("td:nth-child(3) > .pull-right>a:nth-child(1)").text().trim()
+                                    let posted = $(parentChild).find("td:nth-child(3) > div > small > span > span").text().trim()
+                                    let link = $(parentChild).find("td:nth-child(2) > a").attr("href").trim().replace("\n", "")
+                                    const { data } = await axios.get(link)
+                                    const $$ = cheerio.load(data)
+                                    const detailSelector = "#app > div > div > div.col-md-8 > div.panel.panel-default"
+                                    $$(detailSelector).each((childIdx, childEl) => {
+                                          const detail = $$(childEl).find("p").text()
+                                          console.log(detail)
+                                    })
+
+                              }
+
+                        })
+                        currentPage = nextPageNo;
+                  })
+            }
+      } catch (error) {
+
+      }
+
 }
 getCryptoJobs();
