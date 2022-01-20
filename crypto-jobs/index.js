d6f14bdd2b583f1a63b95f7ede3ae192a9422717
sanmak <sanket.mahija@gmail.com>
2022-01-20 12:15:22 +0530

    End loop in crypto job

--- crypto-jobs/index.js
+++ crypto-jobs/index.js
@@ -3,8 +3,8 @@ import * as cheerio from "cheerio"
 
 async function getCryptoJobs() {
       let currentPage = 1;
-
-      for (let i = currentPage; i <= currentPage; i++) {
+      let breakFlag = 0
+      for (let i = currentPage; i <= currentPage && breakFlag === 0; i++) {
             try {
 
 
@@ -16,35 +16,40 @@ async function getCryptoJobs() {
 
                   $(nextpage).each((linkIdx, Linkel) => {
                         console.log("data fetched from:", url);
-                        let nextPageNo = parseInt($(Linkel).find("a.page-link").attr().href.replace("http://crypto.jobs?page=", ""))
+                        let nextPageNo = parseInt($(Linkel)?.find("a.page-link")?.attr()?.href?.replace("http://crypto.jobs?page=", ""))
                         console.log(nextPageNo)
+                        if (!nextPageNo) {
+                              breakFlag = 1
+                        }
+                        else {
+                              $(element).each(async (parentIdx, parentChild) => {
+                                    if (parentIdx > 0) {
+                                          let role = $(parentChild).find("td:nth-child(2) > a > p").text()
+                                          let company = $(parentChild).find("td:nth-child(2) > a > span").text()
+                                          let type = $(parentChild).find("td:nth-child(2) > a > div > small > span:nth-child(1)").text().trim()
+                                          let field = $(parentChild).find("td:nth-child(2) > a > div > small > span:nth-child(2)").text().trim()
+                                          let location = $(parentChild).find("td:nth-child(3) > .pull-right>a:nth-child(1)").text().trim()
+                                          let posted = $(parentChild).find("td:nth-child(3) > div > small > span > span").text().trim()
+                                          let link = $(parentChild).find("td:nth-child(2) > a").attr("href").trim().replace("\n", "")
+                                          const { data } = await axios.get(link)
+                                          const $$ = cheerio.load(data)
+                                          const detailSelector = "#app > div > div > div.col-md-8 > div.panel.panel-default"
+                                          $$(detailSelector).each((childIdx, childEl) => {
+                                                const detail = $$(childEl).find("p").text()
+                                          })
 
-                        $(element).each(async (parentIdx, parentChild) => {
-                              if (parentIdx > 0) {
-                                    let role = $(parentChild).find("td:nth-child(2) > a > p").text()
-                                    let company = $(parentChild).find("td:nth-child(2) > a > span").text()
-                                    let type = $(parentChild).find("td:nth-child(2) > a > div > small > span:nth-child(1)").text().trim()
-                                    let field = $(parentChild).find("td:nth-child(2) > a > div > small > span:nth-child(2)").text().trim()
-                                    let location = $(parentChild).find("td:nth-child(3) > .pull-right>a:nth-child(1)").text().trim()
-                                    let posted = $(parentChild).find("td:nth-child(3) > div > small > span > span").text().trim()
-                                    let link = $(parentChild).find("td:nth-child(2) > a").attr("href").trim().replace("\n", "")
-                                    const { data } = await axios.get(link)
-                                    const $$ = cheerio.load(data)
-                                    const detailSelector = "#app > div > div > div.col-md-8 > div.panel.panel-default"
-                                    $$(detailSelector).each((childIdx, childEl) => {
-                                          const detail = $$(childEl).find("p").text()
-                                    })
+                                    }
 
-                              }
+                              })
 
-                        })
-                        currentPage = nextPageNo;
+                              currentPage = nextPageNo;
+                        }
                   })
 
             } catch (error) {
                   console.error(error)
             }
       }
-
+      console.log("Completed")
 }
 getCryptoJobs();
