a1b586940fea7570dba72e3f4a3dea3a768cffc2
manish <manishvyas393@gmail.com>
2022-01-24 18:07:50 +0530

    data scrapped from daily remote

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 import getCryptoJobs from "./src/cryoto-jobs.js"
 import getJobDetails from "./src/getJobDetails.js"
+import dailyRemote from "./src/dailyRemote.js"
 dotenv.config()
-mongoose.connect(process.env.Db_Url).then(() => {
-getJobDetails().then(()=>getCryptoJobs())
+mongoose.connect(process.env.Db_Url,{ useNewUrlParser: true, useUnifiedTopology: true}).then(() => {
+dailyRemote()
 })
--- src/dailyRemote.js
+++ src/dailyRemote.js
@@ -1,31 +1,74 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-
-async function dailyRemote() {
+import Jobs from "../models/jobs.js"
+export default async function dailyRemote() {
       try {
-            const { data } = await axios.get("https://dailyremote.com/remote-jobs?search=crypto&page=1#main")
-            const $ = cheerio.load(data)
-            const JobList = "#main > div > div > div.content > div.card-container> div:nth-child(3)>article> div.info-container > div > div.profile-information"
-            $(JobList).each(async(parentIdx, parentEl) => {
-                  const role = $(parentEl).find("div> h2 > a").text()
-                  const company = $(parentEl).find("div:nth-child(1) > div.company-name > a").text()
-                  const location = $(parentEl).find("div.job-meta.display-flex.items-center.flex-wrap > span:nth-child(2)>span").text()
-                  const field = $(parentEl).find("div.job-meta.display-flex.items-center.flex-wrap > span.job-category").text().replace("\n", "").trim()
-                  const postedOn = $(parentEl).find("div.job-meta.display-flex.items-center.flex-wrap > span:nth-child(1)").text()
-                  const detailsLink = $(parentEl).find("div> h2 > a").attr("href")
-
-                  const { data } = await axios.get(`https://dailyremote.com${detailsLink}`);
-                  const $$ = cheerio.load(data)
-                  const jobDetail = "body > div.detailed-job-main-container > div.detailed-job-wrapper > div.detailed-job > div.detailed-job-body"
-                  $$(jobDetail).each((childIdx, childEl) => {
-                        const desc = $$(childEl).find("div > div.job-full-description").contents()
-                              .filter(function () {
-                                    return this.type === "text";
-                              }).text()
-                        console.log(desc)
+            let currentPage = 1
+            let breakFlag = 0
+            for (let i = currentPage; i <= currentPage && breakFlag === 0; i++) {
+                  const { data } = await axios.get(`https://dailyremote.com/remote-jobs?search=crypto&page=${currentPage}#main`)
+                  const $ = cheerio.load(data)
+                  const nextPage = "#main > div > div > div.content > div > div:nth-child(4) > div > div"
+                  $(nextPage).each((nxtIdx, nextEl) => {
+                        const nextPageNo = $(nextEl).find("a:last-child").attr("href").replace("/remote-jobs?search=crypto&page=", "").replace("#main", "")
+                        currentPage = nextPageNo;
+                        if (!nextPageNo) {
+                              breakFlag = 1;
+                        }
                   })
-            })
+                  const JobList = "#main > div > div > div.content > div.card-container> div:nth-child(3)>article> div.info-container > div > div.profile-information"
+                  $(JobList).each(async (parentIdx, parentEl) => {
+                        const role = $(parentEl).find("div> h2 > a").text().trim()
+                        const company = $(parentEl).find("div:nth-child(1) > div.company-name > a").text().trim()
+                        const location = $(parentEl).find("div.job-meta.display-flex.items-center.flex-wrap > span:nth-child(2)>span").text()
+                        const field = $(parentEl).find("div.job-meta.display-flex.items-center.flex-wrap > span.job-category").text().replace("\n", "").trim()
+                        const upDatedOn = $(parentEl).find("div.job-meta.display-flex.items-center.flex-wrap > span:nth-child(1)>span:nth-child(2)").text().trim()
+                        const detailsLink = $(parentEl).find("div> h2 > a").attr("href")
+                        const { data } = await axios.get(`https://dailyremote.com${detailsLink}`);
+                        const $$ = cheerio.load(data)
+                        const jobDetail = "body > div.detailed-job-main-container > div.detailed-job-wrapper > div.detailed-job > div.detailed-job-body"
+                        $$(jobDetail).each(async(childIdx, childEl) => {
+                              const details = $$(childEl).find("div > div.job-full-description").html()
+                              await Jobs.findOneAndDelete({ role, company, location, field, upDatedOn }).then(job => {
+                                    if (job) {
+                                          job.remove().then(() => {
+                                                new Jobs({
+                                                      role,
+                                                      company,
+                                                      location,
+                                                      field,
+                                                      upDatedOn,
+                                                      source: detailsLink,
+                                                      details
+                                                      
+                                                }).save().then(() => {
+                                                      console.log("saved")
+                                                })
+                                          })
+                                    }
+                                    else {
+                                          new Jobs({
+                                                role,
+                                                company,
+                                                location,
+                                                field,
+                                                upDatedOn,
+                                                source: detailsLink,
+                                                details
+
+                                          }).save().then(() => {
+                                                console.log("saved")
+                                          })
+                                          
+                                    }
+                              })
+
+                        })
+                  })
+            }
+
       } catch (error) {
+            console.log(error)
 
       }
 }
